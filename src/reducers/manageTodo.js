b2a14c6aec7931bf0dbdfc0918a2e33385753600
rendnil <andrew.lindner.15@gmail.com>
2018-10-29 15:28:46 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,10 +1,17 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  console.log(action);
   switch (action.type) {
     case 'ADD_TODO':
 
       return { todos: state.todos.concat(action.payload.text) };
+
+    case "DELETE_TODO":
+      let newTodos = state.todos.filter((todo)=>{
+        return todo !== action.payload
+      })
+      return {todos: newTodos}
 
     default:
       return state;
